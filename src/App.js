743838e219fa2a97a83a9cce18e7ae60b716b0c4
jiwongil <jiwongil@naver.com>
2023-11-10 13:59:26 +0900

    test

--- src/App.js
+++ src/App.js
@@ -2,14 +2,14 @@ import logo from './logo.svg';
 import { useState } from 'react';
 
 function App() {
-  const han = "안녕";
-  let [listData, setListData] = useState
-  function test() {
-    return 100;
-  }
+  let [listData, setListData] = useState(1)
   return (
     <div className="App">
-      <div>{han}{test()}</div>
+      <div>{listData}</div>
+      
+      <button onClick={()=>{
+        setListData(listData*2)
+      }}>click</button>
     </div>
   );
 }
